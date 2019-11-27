96b6c40ac0283ebff6269cee8015c2b4d6288c4d
Valentin Chobadzhakov <valentin.chobadzhakov@gmail.com>
2019-11-27 17:17:26 +0200

    Router set up

--- burger-project/src/App.js
+++ burger-project/src/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Layout from './hoc/Layouts/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
+
 class App extends React.Component {
 
 
   render() {
     return (
       <div>
-        <Layout>
-          <BurgerBuilder />
-        </Layout>
+        <BrowserRouter>
+          <Layout>
+            <Switch>
+              <Route path="/" exact component={BurgerBuilder} />
+              <Route path="/checkout" component={Checkout} />
+            </Switch>
+          </Layout>
+        </BrowserRouter>
+
       </div >
     );
   }
