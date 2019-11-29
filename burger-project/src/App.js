c18f4f93da956d87369deb395461ba82aa452700
Valentin Chobadzhakov <valentin.chobadzhakov@gmail.com>
2019-11-29 11:55:16 +0200

    Orders page added !

--- burger-project/src/App.js
+++ burger-project/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Layout from './hoc/Layouts/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
+import Orders from './containers/Orders/Orders';
 
 class App extends React.Component {
 
@@ -14,6 +15,7 @@ class App extends React.Component {
           <Layout>
             <Switch>
               <Route path="/" exact component={BurgerBuilder} />
+              <Route path="/orders" component={Orders} />
               <Route path="/checkout" component={Checkout} />
             </Switch>
           </Layout>
